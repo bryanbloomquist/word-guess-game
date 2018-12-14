10517b538958d736ebf7ee6552e6999bc400eefe
bloomy215 <bmbloomquist@gmail.com>
2018-12-14 13:54:58 -0600

    game is 90% complete, need to style it and add game over function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,8 +6,8 @@ var monsters = [
 
 
 //set variable to be filled later
-var wins = 0;
-var losses = 0;
+var level = 0;
+var health = 10;
 var guessesRemaining = 10;
 var lettersUsed = [];
 var answerArray = [];
@@ -19,13 +19,10 @@ function game() {
     var word = monsters[Math.floor(Math.random()*monsters.length)];
         console.log(word);
     //seperate random word into new array
-    letters = word.split("");
-        console.log(letters);
-    //count number of letters in array to set __ in display
-    blanks = letters.length;
-        console.log(blanks);
+    guessWord = word.split("");
+        console.log(guessWord);
     //create __ for each letter in array
-    for (var i = 0; i < blanks; i++) {
+    for (var i = 0; i < guessWord.length; i++) {
         answerArray.push("_");
     }
     //display the word to be guessed in html
@@ -35,17 +32,54 @@ function game() {
 
 //set the reset function
 function reset () {
-    var guessesRemaining = 10;
-    var answerArray = [];
-    var lettersUsed = [];
+    guessesRemaining = 10;
+    lettersUsed = [];
+    answerArray = [];
     game ();
 }
 
 
 //check the user guess with words in the array
+function checkGuess (letter) {
+    var letterMatch = false;
+    for (var i=0; i < guessWord.length; i++) {
+        if (guessWord[i]===letter) {
+            letterMatch = true;
+        }
+    }
+    //if guess is in the word
+    if (letterMatch) {
+        for (var i=0; i < guessWord.length; i++) {
+            if (guessWord[i] == letter) {
+                answerArray[i]=letter;
+            }
+        }
+    }
+    //if guess is not in the word
+    else {
+        lettersUsed.push(letter);
+        guessesRemaining--;
+    }
+}
 
 
 //check if the user won the game
+function gameWin () {
+    //if they won
+    if (guessWord.toString() === answerArray.toString ()) {
+        level++;
+        reset ();
+        document.getElementById("expLevel").innerHTML = +level;
+    //if they lost
+    } else if (guessesRemaining === 0) {
+        health--;
+        reset ();
+        document.getElementById("healthPoints").innerHTML = +health;
+    //if the game continues
+    }
+    document.getElementById("wordSpan").innerHTML = " "+answerArray.join(" ");
+    document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
+}
 
 
 //run the game
@@ -53,3 +87,10 @@ game ();
 
 
 //get the users guess
+document.onkeyup = function(event) {
+    var guess = event.key;
+        console.log(guess);
+    checkGuess(guess);
+    gameWin();
+    document.getElementById("lettersGuessed").innerHTML = " " + lettersUsed.join("  ");
+}
